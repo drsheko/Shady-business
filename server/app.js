b49927246620f7418be85ad4bcf0e4be5b43c7db
drsheko <drsheko91@gmail.com>
2023-03-27 01:34:59 -0500

    setup mongoDB connection

--- server/app.js
+++ server/app.js
@@ -1,26 +1,43 @@
+require("dotenv").config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require("mongoose");
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var Routers = require("./routes/routers");
+var bcrypt = require("bcryptjs");
+var cors = require("cors");
+const multer = require("multer");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Mongo_DB Setup
+mongoDB =
 
+  "mongodb+srv://drsheko91:"
+  +process.env.MONGO_PASSCODE+
+  "@shady-business.tguywpm.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Mongo Connection Error"));
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Routes setup
+app.use("/", Routers);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
