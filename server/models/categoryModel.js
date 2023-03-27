dad57c818351372707ee1c116b0b2f0d403972ac
drsheko <drsheko91@gmail.com>
2023-03-27 15:57:34 -0500

    create category model

---
+++ server/models/categoryModel.js
@@ -0,0 +1,12 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+const categorySchema = new Schema({
+  name: { type: String, required: true },
+  sub_categories: [
+    { type: Schema.Types.ObjectId, ref: "subCategory", default: [] },
+  ],
+  deals: [{ type: Schema.Types.ObjectId, ref: "deal", default: [] }],
+});
+
+module.exports = mongoose.model("category", categorySchema, "categories");
