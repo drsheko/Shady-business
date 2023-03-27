746ff8ce6255654f7c4aecdaa5d139cfdde2bbc9
drsheko <drsheko91@gmail.com>
2023-03-27 15:57:53 -0500

    create sub category model

---
+++ server/models/sub_categoryModel.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+const subCategorySchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  photo: { type: String },
+  category: [{ type: Schema.Types.ObjectId, ref: "category", default: [] }],
+  products: [{ type: Schema.Types.ObjectId, ref: "product", default: [] }],
+  deals: [{ type: Schema.Types.ObjectId, ref: "deal", default: [] }],
+});
+
+module.exports = mongoose.model(
+  "subCategory",
+  subCategorySchema,
+  "subCategories"
+);
