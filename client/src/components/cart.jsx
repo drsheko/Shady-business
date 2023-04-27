6d4dc6551cf41500f5df3b06ee28af722977de82
drsheko <drsheko91@gmail.com>
2023-04-27 12:44:19 -0500

    edit cart component

--- client/src/components/cart.jsx
+++ client/src/components/cart.jsx
@@ -1,28 +1,19 @@
-import React , {useState, useContext} from 'react';
+import { Button } from "primereact/button";
+import React, { useState, useContext } from "react";
+import { InputNumber } from "primereact/inputnumber";
+import CartItem from "./cartItem";
 
 function Cart(props) {
-    return (
-        <div className='p-2'>
-            <div className="page-title flex ">
-                <p className="text-2xl mx-auto">Your Cart (5)</p>
-            </div>
-            <div className="cart-items flex-column">
-                <div className="cart-item">
-                    <div className="cart-item-figure">
-                        <img src="../src/assets/vape/vape1.avif" alt=""  width='100px' height={"100px"}/>
-                    </div>
-                    <div className="cart-item-info flex flex-column">
-                        <p className="cart-item-category text-lg text-400">
-                            POD POCKET
-                        </p>
-                        <p className="cart-item-name text-xl link">
-                        Pod Pocket Disposable (7500 Puffs)
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="p-2">
+      <div className="page-title flex ">
+        <p className="text-2xl mx-auto">Your Cart (5)</p>
+      </div>
+      <div className="cart-items flex flex-column">
+        <CartItem/>
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
