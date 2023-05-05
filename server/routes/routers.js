68cd3132b012b0b3b325e110c392d51a946b649c
drsheko <drsheko91@gmail.com>
2023-05-04 23:46:58 -0500

    add sign up route

--- server/routes/routers.js
+++ server/routes/routers.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var UserController = require('../controllers/userController')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+// Sign Up
+router.post('/api/signup',UserController.signup_post )
 module.exports = router;
