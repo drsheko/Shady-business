3462f8d4282b71fdc2e4a033d7b643119ea10f26
drsheko <drsheko91@gmail.com>
2023-05-05 00:46:22 -0500

    edit user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,14 +2,13 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: { type: String, minLength: 3, required: true },
   firstName: { type: String, required: true },
-  lasttName: { type: String, required: true },
+  lastName: { type: String, required: true },
   email: { type: String, required: true },
-  phone: { type: Number, required: true },
+  phone: { type: String, required: true },
   password: { type: String, minLength: 6, required: true },
   dateOfBirth: { type: Date, required: true },
-  address: { type: String, required: true },
+  address: [{ type:  Schema.Types.ObjectId, ref: "address", default: [] }],
   status: { type: String, required: true, default: "member" }, // memeber // admin // manager
   orders: [{ type: Schema.Types.ObjectId, ref: "order", default: [] }],
   lists: {
@@ -18,7 +17,7 @@ const userSchema = new Schema({
     saved: [],
   },
   reviews: [{ type: Schema.Types.ObjectId, ref: "review", default: [] }],
-  payment: [{ type: Schema.Types.ObjectId, ref: "payment", default: [] }],
+  payments: [{ type: Schema.Types.ObjectId, ref: "payment", default: [] }],
 });
 
 module.exports = mongoose.model("user", userSchema, "users");
