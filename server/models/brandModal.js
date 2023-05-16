9c5c110cc845389531647ccbf802077d6d09b56e
drsheko <drsheko91@gmail.com>
2023-05-16 14:28:52 -0500

    add brand model

---
+++ server/models/brandModal.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+const brandSchema = new Schema({
+  name: { type: String, required: true },
+  photo:{type:String },
+  products: [
+    { type: Schema.Types.ObjectId, ref: "product", default: [] },
+  ],
+  deals: [{ type: Schema.Types.ObjectId, ref: "deal", default: [] }],
+});
+
+module.exports = mongoose.model("brand", brandSchema, "brands");
