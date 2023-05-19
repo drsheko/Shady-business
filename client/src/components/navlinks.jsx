ec47862eac8eb8a367a51ae7e43e6b813eb1291d
drsheko <drsheko91@gmail.com>
2023-05-18 23:28:04 -0500

    add dynamic nav links

--- client/src/components/navlinks.jsx
+++ client/src/components/navlinks.jsx
@@ -1,139 +1,58 @@
-import React from 'react';
-import { Menubar } from 'primereact/menubar';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Menubar } from "primereact/menubar";
 
 function NavLinks(props) {
-  const items = [
-    {
-        label: 'File',
-        icon: 'pi pi-fw pi-file',
-        items: [
-            {
-                label: 'New',
-                icon: 'pi pi-fw pi-plus',
-                items: [
-                    {
-                        label: 'Bookmark',
-                        icon: 'pi pi-fw pi-bookmark'
-                    },
-                    {
-                        label: 'Video',
-                        icon: 'pi pi-fw pi-video'
-                    },
+  const brands = {
+    label: "Brands",
+    url: "/brands",
+  };
+  const [links, setLinks] = useState([brands]);
 
-                ]
-            },
-            {
-                label: 'Delete',
-                icon: 'pi pi-fw pi-trash'
-            },
-            {
-                separator: true
-            },
-            {
-                label: 'Export',
-                icon: 'pi pi-fw pi-external-link'
-            }
-        ]
-    },
-    {
-        label: 'Edit',
-        icon: 'pi pi-fw pi-pencil',
-        items: [
-            {
-                label: 'Left',
-                icon: 'pi pi-fw pi-align-left'
-            },
-            {
-                label: 'Right',
-                icon: 'pi pi-fw pi-align-right'
-            },
-            {
-                label: 'Center',
-                icon: 'pi pi-fw pi-align-center'
-            },
-            {
-                label: 'Justify',
-                icon: 'pi pi-fw pi-align-justify'
-            },
+  useEffect(() => {
+    // get All Categories ;
+    const getAllCategories = async () => {
+      let url = "http://localhost:3000/api/categories/all";
+      try {
+        let res = await axios.get(url);
+        let categoryLinks = res.data.allCategories;
+        let updated = categoryLinks.map((category) => {
+          if (category.sub_categories.length === 0) {
+            category = {
+              id: category._id,
+              label: category.name,
+            };
+            return category;
+          } else {
+            let sub = category.sub_categories;
+            let subLinks = sub.map((el) => {
+              el = {
+                id: el._id,
+                label: el.name,
+              };
+              return el;
+            });
+            category = {
+              id: category._id,
+              label: category.name,
+              items: subLinks,
+            };
+            return category;
+          }
+        });
 
-        ]
-    },
-    {
-        label: 'Users',
-        icon: 'pi pi-fw pi-user',
-        items: [
-            {
-                label: 'New',
-                icon: 'pi pi-fw pi-user-plus',
+        setLinks([brands, ...updated]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-            },
-            {
-                label: 'Delete',
-                icon: 'pi pi-fw pi-user-minus',
-
-            },
-            {
-                label: 'Search',
-                icon: 'pi pi-fw pi-users',
-                items: [
-                    {
-                        label: 'Filter',
-                        icon: 'pi pi-fw pi-filter',
-                        items: [
-                            {
-                                label: 'Print',
-                                icon: 'pi pi-fw pi-print'
-                            }
-                        ]
-                    },
-                    {
-                        icon: 'pi pi-fw pi-bars',
-                        label: 'List'
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        label: 'Events',
-        icon: 'pi pi-fw pi-calendar',
-        items: [
-            {
-                label: 'Edit',
-                icon: 'pi pi-fw pi-pencil',
-                items: [
-                    {
-                        label: 'Save',
-                        icon: 'pi pi-fw pi-calendar-plus'
-                    },
-                    {
-                        label: 'Delete',
-                        icon: 'pi pi-fw pi-calendar-minus'
-                    }
-                ]
-            },
-            {
-                label: 'Archive',
-                icon: 'pi pi-fw pi-calendar-times',
-                items: [
-                    {
-                        label: 'Remove',
-                        icon: 'pi pi-fw pi-calendar-minus'
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        label: 'Quit',
-        icon: 'pi pi-fw pi-power-off'
-    }
-];
+    getAllCategories();
+  }, []);
   return (
     <div className="card">
-            <Menubar model={items} breakpoint="360px" />
-        </div>
+      <Menubar model={links} breakpoint="360px" />
+    </div>
   );
 }
 
