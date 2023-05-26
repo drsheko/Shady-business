309b3f4dc4690b8e20d7375d90516ff448c174bf
drsheko <drsheko91@gmail.com>
2023-05-25 20:40:55 -0500

    update product model

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -5,8 +5,7 @@ const productSchema = new Schema({
   name: { type: String, minLength: 3, required: true },
   price: { type: Number, required: true },
   retail_price: { type: Number, required: true },
-  total_quantity: { type: Number },
-  total_stock: { type: Number },
+  total_stock: { type: Number, default:0 },
   brand: { type: Schema.Types.ObjectId, ref: "brand" },
   description: { type: String, required: true },
   photos: { type: Array, default: [] },
@@ -17,11 +16,15 @@ const productSchema = new Schema({
     { type: Schema.Types.ObjectId, ref: "productOption", default: [] },
   ],
   reviews: [{ type: Schema.Types.ObjectId, ref: "review", default: [] }],
+  rating:{type:Number, default:0},
   deals: [{ type: Schema.Types.ObjectId, ref: "deal", default: [] }],
-  subCategory: [
-    { type: Schema.Types.ObjectId, ref: "subCategory", default: [] },
-  ],
-  category: [{ type: Schema.Types.ObjectId, ref: "category", default: [] }],
+  subCategory: 
+    { type: Schema.Types.ObjectId, ref: "subCategory"},
+  
+  category: { type: Schema.Types.ObjectId, ref: "category" },
+
 });
+
+
 
 module.exports = mongoose.model("product", productSchema, "products");
