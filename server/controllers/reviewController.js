76fe05c63a07c549e3f12f2e919e14218d9c6abc
drsheko <drsheko91@gmail.com>
2023-05-30 22:24:51 -0500

    add create review API

--- server/controllers/reviewController.js
+++ server/controllers/reviewController.js
@@ -9,31 +9,38 @@ const upload = multer({ memoStorage });
 
 // Create A Review
 exports.CREATE_Review = [
-  upload.single("photo"),
+  upload.any(),
   async (req, res) => {
-    var uploadedFileURL;
-
     // upload review image
-    const file = req.file;
-    if (file) {
-      const fileName = file.originalname + new Date();
-      const imageRef = ref(storage, fileName);
-      const metatype = { contentType: file.mimetype, name: file.originalname };
-      await uploadBytes(imageRef, file.buffer, metatype)
-        .then((snapshot) => {
-          uploadedFileURL = `https://firebasestorage.googleapis.com/v0/b/${snapshot.ref._location.bucket}/o/${snapshot.ref._location.path_}?alt=media`;
-        })
-        .catch((error) => {
-          return res.status(401).json({ error: error });
-        });
+    var uploadedFileURL = [];
+    for (let i = 0; i < req.files.length; i++) {
+      const file = req.files[i];
+      if (file) {
+        const fileName = file.originalname + new Date();
+        const imageRef = ref(storage, fileName);
+        const metatype = {
+          contentType: file.mimetype,
+          name: file.originalname,
+        };
+        await uploadBytes(imageRef, file.buffer, metatype)
+          .then((snapshot) => {
+            uploadedFileURL.push(
+              `https://firebasestorage.googleapis.com/v0/b/${snapshot.ref._location.bucket}/o/${snapshot.ref._location.path_}?alt=media`
+            );
+          })
+          .catch((error) => {
+            return res.status(401).json({ error: error });
+          });
+      }
     }
+
     // create a new review
     let newReview = new Review({
       user: req.body.user,
       product: req.body.product,
       rating: req.body.rating,
       comment: req.body.comment,
-      photo: uploadedFileURL,
+      photos: uploadedFileURL,
     });
 
     try {
