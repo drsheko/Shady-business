292ce0d5ddbd00badb3b86007c0f7438d9f5ff4c
drsheko <drsheko91@gmail.com>
2023-06-12 13:23:22 -0500

    add dynamic data to category

--- client/src/components/category.jsx
+++ client/src/components/category.jsx
@@ -1,126 +1,126 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import {BreadCrumb} from 'primereact/breadcrumb';
-import { Link } from 'react-router-dom'; 
-import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
-import { Button } from 'primereact/button';
-import { Dropdown } from 'primereact/dropdown';
-import { Rating } from 'primereact/rating';
-import ProductCard from './productCard';
+import React from "react";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+import { BreadCrumb } from "primereact/breadcrumb";
+import { Link } from "react-router-dom";
+import { DataView, DataViewLayoutOptions } from "primereact/dataview";
+import { Dropdown } from "primereact/dropdown";
+import ProductCard from "./productCard";
+
 function Category(props) {
-    const item = (i, op) =>{return (<Link to='/'>home </Link>)} 
-    const items = [{ label: 'Computer', template:item }];
-    const home={ icon: 'pi pi-home', template:item }
-    //---------------------
-    //const [products, setProducts] = useState(null);
-    const [layout, setLayout] = useState('grid');
-    const [sortKey, setSortKey] = useState(null);
-    const [sortOrder, setSortOrder] = useState(null);
-    const [sortField, setSortField] = useState(null);
-    const sortOptions = [
-        {label: 'Price High to Low', value: '!price'},
-        {label: 'Price Low to High', value: 'price'},
-        {label: 'Highest Rating', value: '!rating'},
-        {label: 'A to Z', value: 'name'},
-        {label: 'Z to A', value: '!name'},
-        {label: 'Default', value: 'default'},
-    ];
+  const category = useLocation().state;
+  const [products, setProducts] = useState([]);
+  const item = (i, op) => {
+    return <Link to="/">home </Link>;
+  };
+  const items = [{ label: "Computer", template: item }];
+  const home = { icon: "pi pi-home", template: item };
+  const [layout, setLayout] = useState("grid");
+  const [sortKey, setSortKey] = useState(null);
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortField, setSortField] = useState(null);
+  const sortOptions = [
+    { label: "Price High to Low", value: "!price" },
+    { label: "Price Low to High", value: "price" },
+    { label: "Highest Rating", value: "!rating" },
+    { label: "A to Z", value: "name" },
+    { label: "Z to A", value: "!name" },
+    { label: "Default", value: "default" },
+  ];
 
-        
-            const products = [
-                {"id": "1000","code": "f230fh0g3","name": "Bamboo Watch","description": "Product Description","image": "vape1.avif","price": 65,"category": "Accessories","quantity": 24,"inventoryStatus": "INSTOCK","rating": 5},
-                {"id": "1001","code": "nvklal433","name": "Black Watch","description": "Product Description","image": "vape2.avif","price": 72,"category": "Accessories","quantity": 61,"inventoryStatus": "INSTOCK","rating": 4},
-                {"id": "1002","code": "zz21cz3c1","name": "Blue Band","description": "Product Description","image": "vape3.avif","price": 79,"category": "Fitness","quantity": 2,"inventoryStatus": "LOWSTOCK","rating": 3},
-                {"id": "1003","code": "244wgerg2","name": "Blue T-Shirt","description": "Product Description","image": "blue-t-shirt.jpg","price": 29,"category": "Clothing","quantity": 25,"inventoryStatus": "INSTOCK","rating": 5},
-                {"id": "1004","code": "h456wer53","name": "Bracelet","description": "Product Description","image": "bracelet.jpg","price": 15,"category": "Accessories","quantity": 73,"inventoryStatus": "INSTOCK","rating": 4},
-                {"id": "1005","code": "av2231fwg","name": "Brown Purse","description": "Product Description","image": "brown-purse.jpg","price": 120,"category": "Accessories","quantity": 0,"inventoryStatus": "OUTOFSTOCK","rating": 4},
-                {"id": "1006","code": "bib36pfvm","name": "Chakra Bracelet","description": "Product Description","image": "chakra-bracelet.jpg","price": 32,"category": "Accessories","quantity": 5,"inventoryStatus": "LOWSTOCK","rating": 3},
-                {"id": "1007","code": "mbvjkgip5","name": "Galaxy Earrings","description": "Product Description","image": "galaxy-earrings.jpg","price": 34,"category": "Accessories","quantity": 23,"inventoryStatus": "INSTOCK","rating": 5},
-                {"id": "1008","code": "vbb124btr","name": "Game Controller","description": "Product Description","image": "game-controller.jpg","price": 99,"category": "Electronics","quantity": 2,"inventoryStatus": "LOWSTOCK","rating": 4},
-                {"id": "1009","code": "cm230f032","name": "Gaming Set","description": "Product Description","image": "gaming-set.jpg","price": 299,"category": "Electronics","quantity": 63,"inventoryStatus": "INSTOCK","rating": 3},
-                {"id": "1010","code": "plb34234v","name": "Gold Phone Case","description": "Product Description","image": "gold-phone-case.jpg","price": 24,"category": "Accessories","quantity": 0,"inventoryStatus": "OUTOFSTOCK","rating": 4},
-                {"id": "1011","code": "4920nnc2d","name": "Green Earbuds","description": "Product Description","image": "green-earbuds.jpg","price": 89,"category": "Electronics","quantity": 23,"inventoryStatus": "INSTOCK","rating": 4},
-                {"id": "1012","code": "250vm23cc","name": "Green T-Shirt","description": "Product Description","image": "green-t-shirt.jpg","price": 49,"category": "Clothing","quantity": 74,"inventoryStatus": "INSTOCK","rating": 5},
-                {"id": "1013","code": "fldsmn31b","name": "Grey T-Shirt","description": "Product Description","image": "grey-t-shirt.jpg","price": 48,"category": "Clothing","quantity": 0,"inventoryStatus": "OUTOFSTOCK","rating": 3},
-                {"id": "1014","code": "waas1x2as","name": "Headphones","description": "Product Description","image": "headphones.jpg","price": 175,"category": "Electronics","quantity": 8,"inventoryStatus": "LOWSTOCK","rating": 5},
-                {"id": "1015","code": "vb34btbg5","name": "Light Green T-Shirt","description": "Product Description","image": "light-green-t-shirt.jpg","price": 49,"category": "Clothing","quantity": 34,"inventoryStatus": "INSTOCK","rating": 4},
-                {"id": "1016","code": "k8l6j58jl","name": "Lime Band","description": "Product Description","image": "lime-band.jpg","price": 79,"category": "Fitness","quantity": 12,"inventoryStatus": "INSTOCK","rating": 3},
-                {"id": "1017","code": "v435nn85n","name": "Mini Speakers","description": "Product Description","image": "mini-speakers.jpg","price": 85,"category": "Clothing","quantity": 42,"inventoryStatus": "INSTOCK","rating": 4},
-                {"id": "1018","code": "09zx9c0zc","name": "Painted Phone Case","description": "Product Description","image": "painted-phone-case.jpg","price": 56,"category": "Accessories","quantity": 41,"inventoryStatus": "INSTOCK","rating": 5},
-                {"id": "1019","code": "mnb5mb2m5","name": "Pink Band","description": "Product Description","image": "pink-band.jpg","price": 79,"category": "Fitness","quantity": 63,"inventoryStatus": "INSTOCK","rating": 4},
-                {"id": "1020","code": "r23fwf2w3","name": "Pink Purse","description": "Product Description","image": "pink-purse.jpg","price": 110,"category": "Accessories","quantity": 0,"inventoryStatus": "OUTOFSTOCK","rating": 4},
-                {"id": "1021","code": "pxpzczo23","name": "Purple Band","description": "Product Description","image": "purple-band.jpg","price": 79,"category": "Fitness","quantity": 6,"inventoryStatus": "LOWSTOCK","rating": 3},
-                {"id": "1022","code": "2c42cb5cb","name": "Purple Gemstone Necklace","description": "Product Description","image": "purple-gemstone-necklace.jpg","price": 45,"category": "Accessories","quantity": 62,"inventoryStatus": "INSTOCK","rating": 4},
-                {"id": "1023","code": "5k43kkk23","name": "Purple T-Shirt","description": "Product Description","image": "purple-t-shirt.jpg","price": 49,"category": "Clothing","quantity": 2,"inventoryStatus": "LOWSTOCK","rating": 5},
-                {"id": "1024","code": "lm2tny2k4","name": "Shoes","description": "Product Description","image": "shoes.jpg","price": 64,"category": "Clothing","quantity": 0,"inventoryStatus": "INSTOCK","rating": 4},
-                {"id": "1025","code": "nbm5mv45n","name": "Sneakers","description": "Product Description","image": "sneakers.jpg","price": 78,"category": "Clothing","quantity": 52,"inventoryStatus": "INSTOCK","rating": 4},
-                {"id": "1026","code": "zx23zc42c","name": "Teal T-Shirt","description": "Product Description","image": "teal-t-shirt.jpg","price": 49,"category": "Clothing","quantity": 3,"inventoryStatus": "LOWSTOCK","rating": 3},
-                {"id": "1027","code": "acvx872gc","name": "Yellow Earbuds","description": "Product Description","image": "yellow-earbuds.jpg","price": 89,"category": "Electronics","quantity": 35,"inventoryStatus": "INSTOCK","rating": 3},
-                {"id": "1028","code": "tx125ck42","name": "Yoga Mat","description": "Product Description","image": "yoga-mat.jpg","price": 20,"category": "Fitness","quantity": 15,"inventoryStatus": "INSTOCK","rating": 5},
-                {"id": "1029","code": "gwuby345v","name": "Yoga Set","description": "Product Description","image": "yoga-set.jpg","price": 20,"category": "Fitness","quantity": 25,"inventoryStatus": "INSTOCK","rating": 8}
-            ]
-           
-        const onSortChange = (event) => {
-            const value = event.value;
-           if(value==='default'){
-                setSortField(null);
-                setSortKey(null);
-                return
-           }
-            if (value.indexOf('!') === 0) {
-                setSortOrder(-1);
-                setSortField(value.substring(1, value.length));
-                setSortKey(value);
-            }
-            else {
-                setSortOrder(1);
-                setSortField(value);
-                setSortKey(value);
-            }
-        }
+  const onSortChange = (event) => {
+    const value = event.value;
+    if (value === "default") {
+      setSortField(null);
+      setSortKey(null);
+      return;
+    }
+    if (value.indexOf("!") === 0) {
+      setSortOrder(-1);
+      setSortField(value.substring(1, value.length));
+      setSortKey(value);
+    } else {
+      setSortOrder(1);
+      setSortField(value);
+      setSortKey(value);
+    }
+  };
 
-        const itemTemplate = (product, layout) => {
-            if (!product) {
-                return;
-            }
-    
-            if (layout === 'list')
-                return <ProductCard product={product} layout={layout}/>;
-            else if (layout === 'grid')
-                return <ProductCard product={product} layout={layout}/>;
-                ;
-        }
+  const itemTemplate = (product, layout) => {
+    if (!product) {
+      return;
+    }
+    if (layout === "list")
+      return <ProductCard product={product} layout={layout} />;
+    else if (layout === "grid")
+      return <ProductCard product={product} layout={layout} />;
+  };
 
-        const renderHeader = () => {
-            return (
-                <div className="grid grid-nogutter">
-                    <div className="col-6 flex" style={{textAlign: 'left'}}>
-                        <p className='align-self-center mr-2'>Sort</p>
-                        <Dropdown options={sortOptions} value={sortKey} optionLabel="label" placeholder=" By" onChange={onSortChange}/>
-                    </div>
-                    <div className="col-6" style={{textAlign: 'right'}}>
-                        <DataViewLayoutOptions layout={layout} onChange={(e) => setLayout(e.value)} />
-                    </div>
-                </div>
-            );
-        }
-    
-        const header = renderHeader();
+  const renderHeader = () => {
     return (
-        <div>
-           <BreadCrumb model={items} home={home} /> 
-            <div className='card'>
-                <p className=' text-center text-4xl font-medium capitalize my-2'> vape tanks</p>
-            </div>
-            <div className="dataview-demo">
-            <div className="card">
-                <DataView value={products} layout={layout} header={header}
-                        itemTemplate={itemTemplate} paginator rows={12}
-                        sortOrder={sortOrder} sortField={sortField} />
-            </div>
+      <div className="grid grid-nogutter w-full">
+        <div className="col-6 flex" style={{ textAlign: "left" }}>
+          <p className="align-self-center mr-2">Sort</p>
+          <Dropdown
+            options={sortOptions}
+            value={sortKey}
+            optionLabel="label"
+            placeholder=" By"
+            onChange={onSortChange}
+          />
         </div>
+        <div className="col-6" style={{ textAlign: "right" }}>
+          <DataViewLayoutOptions
+            layout={layout}
+            onChange={(e) => setLayout(e.value)}
+          />
         </div>
+      </div>
     );
+  };
+
+  const header = renderHeader();
+  useEffect(() => {
+    const getData = async () => {
+      let url = "http://localhost:3000/api/categories/category";
+      try {
+        let id = category._id;
+        let res = await axios.post(url, { id });
+        setProducts(res.data.category.products);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+    console.log(category);
+  }, []);
+
+  return (
+    <div>
+      <BreadCrumb model={items} home={home} />
+      <div className="card">
+        <p className=" text-center text-4xl font-medium capitalize my-2">
+          {" "}
+          vape tanks
+        </p>
+      </div>
+      <div className="dataview-demo flex flex-row">
+        <div className="card w-full">
+          <DataView
+            value={products}
+            layout={layout}
+            header={header}
+            itemTemplate={itemTemplate}
+            paginator
+            rows={12}
+            sortOrder={sortOrder}
+            sortField={sortField}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Category;
