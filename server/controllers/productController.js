12866d3b2e8bc3c96cb2161aa8b2d28f86abc8e5
drsheko <drsheko91@gmail.com>
2023-06-29 20:47:50 -0500

    add product option controller

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,17 +1,18 @@
 var Product = require("../models/productModel");
 var Option = require("../models/productOption");
+var Category = require("../models/categoryModel");
 var SubCategory = require("../models/sub_categoryModel");
+var Review =require('../models/reviewModel');
 const multer = require("multer");
-const { ref, uploadBytes } = require("firebase/storage");
+const { ref, uploadBytes, deleteObject } = require("firebase/storage");
 const storage = require("../firebase");
 const memoStorage = multer.memoryStorage();
 const upload = multer({ memoStorage });
 
-// CREATE PRODUCT
+//CREATE PRODUCT
 exports.CREATE_PRODUCT = [
-  upload.single("photo"),
+  upload.any("photos"),
   async (req, res) => {
-    var uploadedFileURL;
     const isNameTaken = await Product.findOne({ name: req.body.name });
     if (isNameTaken != null) {
       return res.status(401).json({
@@ -19,22 +20,31 @@ exports.CREATE_PRODUCT = [
         error: "This Product is already in Database",
       });
     }
-    var file = req.file;
-    if (file) {
-      const fileName = file.originalname + new Date();
-      const imageRef = ref(storage, fileName);
-      const metatype = { contentType: file.mimetype, name: file.originalname };
-      await uploadBytes(imageRef, file.buffer, metatype)
-        .then((snapshot) => {
-          uploadedFileURL = `https://firebasestorage.googleapis.com/v0/b/${snapshot.ref._location.bucket}/o/${snapshot.ref._location.path_}?alt=media`;
-        })
-        .catch((error) => {
-          return res.status(401).json({ error: error });
-        });
-    } else {
+    var uploadedFileURL = [];
+    for (let i = 0; i < req.files.length; i++) {
+      const file = req.files[i];
+      if (file) {
+        const fileName = file.originalname + new Date();
+        const imageRef = ref(storage, fileName);
+        const metatype = {
+          contentType: file.mimetype,
+          name: file.originalname,
+        };
+        await uploadBytes(imageRef, file.buffer, metatype)
+          .then((snapshot) => {
+            uploadedFileURL.push(
+              `https://firebasestorage.googleapis.com/v0/b/${snapshot.ref._location.bucket}/o/${snapshot.ref._location.path_}?alt=media`
+            );
+          })
+          .catch((error) => {
+            return res.status(401).json({ error: error });
+          });
+      }
+    }
+    if (uploadedFileURL.length === 0) {
       return res
         .status(401)
-        .json({ success: false, error: " Product photo is required !!" });
+        .json({ success: false, error: "Product photo is required !!" });
     }
     let newProduct = new Product({
       name: req.body.name,
@@ -48,30 +58,181 @@ exports.CREATE_PRODUCT = [
       photos: uploadedFileURL,
       related_products: req.body.related_products,
     });
-
-    newProduct
-      .save()
-      .then((product) => {
-        SubCategory.findByIdAndUpdate(req.body.subCategory, {
-          $push: {
-            products: product._id,
-          },
-        }).then(() => {
-          return res.status(200).json({ success: true, product });
-        });
-      })
-      .catch((error) => {
-        return res.status(401).json({ success: false, error });
+    try {
+      let createdProduct = await newProduct.save();
+      let product = await createdProduct.populate([
+        "category",
+        "subCategory",
+        "brand",
+        "related_products",
+      ]);
+      await Category.findByIdAndUpdate(req.body.category, {
+        $push: {
+          products: createdProduct._id,
+        },
       });
+      await SubCategory.findByIdAndUpdate(req.body.subCategory, {
+        $push: {
+          products: createdProduct._id,
+        },
+      });
+
+      return res.status(200).json({ success: true, product });
+    } catch (error) {
+      console.log("last", error);
+      return res.status(401).json({ success: false, error });
+    }
   },
 ];
 
-// GET PRODUCT BY ID 
-exports.GET_PRODUCT_By_Id = async(req, res) => {
-  try{
-    let product = await Product.findById(req.body.id).populate(['options', 'subCategory']);
-    return res.status(200).json({success:true, product})
-  }catch(error){
-    return res.status(401).json({success:false, error})
+// EDIT PRODUCT
+exports.EDIT_PRODUCT = [
+  upload.any("photos"),
+  async (req, res) => {
+    var uploadedFilesURL = [];
+    for (let i = 0; i < req.files.length; i++) {
+      const file = req.files[i];
+      if (file) {
+        const fileName = file.originalname + new Date();
+        const imageRef = ref(storage, fileName);
+        const metatype = {
+          contentType: file.mimetype,
+          name: file.originalname,
+        };
+        await uploadBytes(imageRef, file.buffer, metatype)
+          .then((snapshot) => {
+            uploadedFilesURL.push(
+              `https://firebasestorage.googleapis.com/v0/b/${snapshot.ref._location.bucket}/o/${snapshot.ref._location.path_}?alt=media`
+            );
+          })
+          .catch((error) => {
+            return res.status(401).json({ error });
+          });
+      }
+    }
+    try {
+      let id = req.body._id;
+      let editedProduct = await Product.findByIdAndUpdate(
+        id,
+        {
+          $set: {
+            name: req.body.name,
+            price: req.body.price,
+            retail_price: req.body.retail,
+            total_stock: req.body.stock,
+            brand: req.body.brand,
+            subCategory: req.body.subCategory,
+            category: req.body.category,
+            description: req.body.description,
+            related_products: req.body.related_products,
+          },
+          $push: {
+            photos: {
+              $each: uploadedFilesURL,
+            },
+          },
+        },
+        { new: true }
+      );
+      let product = await editedProduct.populate([
+        "options",
+        "category",
+        "subCategory",
+        "brand",
+        "related_products",
+      ]);
+      return res.status(200).json({ success: true, product });
+    } catch (error) {
+      return res.status(401).json({ success: false, error });
+    }
+  },
+];
+
+// GET PRODUCT BY ID
+exports.GET_PRODUCT_By_Id = async (req, res) => {
+  try {
+    let product = await Product.findById(req.body.id).populate([
+      "options",
+      "subCategory",
+      "category",
+      "brand",
+      "related_products",
+    ]);
+    return res.status(200).json({ success: true, product });
+  } catch (error) {
+    return res.status(401).json({ success: false, error });
   }
-}
+};
+
+// GET ALL PRODUCTS
+exports.GET_All_PRODUCTS = async (req, res) => {
+  try {
+    let products = await Product.find().populate([
+      "options",
+      "subCategory",
+      "category",
+      "brand",
+      "related_products",
+    ]);
+    return res.status(200).json({ success: true, products });
+  } catch (error) {
+    return res.status(401).json({ success: false, error });
+  }
+};
+
+// DELETE PRODUCT BY Id
+exports.DELETE_PRODUCT_BY_ID = async (req, res) => { 
+  let id = req.body.id;
+  let photos = req.body.photos;
+
+  try {
+    // delete product's photos 
+     await photos.map(photo =>{
+        const fileRef = ref(storage,photo)
+        deleteObject(fileRef)
+      })
+      // delete product
+    await Product.findByIdAndDelete(id);
+    // delete product's options 
+    await Option.deleteMany({product:id});
+      // delete product reviews
+      await Review.deleteMany({product:id})
+    // delete product  from category
+    await Category.updateMany({},{
+      $pull:{
+        products:id
+      }
+    });
+     // delete product  from subcategory
+    await SubCategory.updateMany({},{
+      $pull:{
+        products:id
+      }
+    });
+  
+    return res.status(200).json({ success: true });
+  } catch (error) {
+    return res.status(401).json({ success: false, error });
+  }
+};
+
+exports.edit = async (req, res) => {
+  Product.updateMany(
+    {},
+    {
+      $set: {
+        category: req.body.category,
+        subCategory: req.body.sub,
+      },
+    },
+    {
+      new: true,
+    }
+  )
+    .then((products) => {
+      return res.status(200).json({ success: true, products });
+    })
+    .catch((error) => {
+      return res.status(401).json({ success: false, error });
+    });
+};
