47cd76e3d03bd906b4c0dd6037b2c0fd532b1632
drsheko <drsheko91@gmail.com>
2023-08-28 01:58:35 -0500

    fix sign-in UI

--- client/src/components/sign-in-sidebar.jsx
+++ client/src/components/sign-in-sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState , useRef} from "react";
+import React, { useState, useRef, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { Sidebar } from "primereact/sidebar";
@@ -6,13 +6,14 @@ import { Button } from "primereact/button";
 import { Checkbox } from "primereact/checkbox";
 import { InputText } from "primereact/inputtext";
 import { Password } from "primereact/password";
-import { Messages } from 'primereact/messages';
-        
+import { Messages } from "primereact/messages";
+import { UserContext } from "../App";
+
 function SignInSidebar(props) {
   const [checked, setChecked] = useState(false);
   let navigate = useNavigate();
-  let msgs = useRef(null)
-  const [error, setError] = useState('');
+  let { user, setUser } = useContext(UserContext);
+  let msgs = useRef(null);
   const [form, setForm] = useState({
     email: "",
     password: "",
@@ -23,7 +24,7 @@ function SignInSidebar(props) {
   };
 
   const handleFormSubmit = async (e) => {
-    msgs.current.clear()
+    msgs.current.clear();
     try {
       e.preventDefault();
       var res = await axios.post("http://localhost:3000/api/login", {
@@ -33,17 +34,35 @@ function SignInSidebar(props) {
       let success = res.data.success;
       if (success) {
         props.setSignInVisible(false);
-        var user = res.data.user;
+        var loggedUser = res.data.user;
         // Save logged user to local storage
-       
+        localStorage.setItem("SHADY_BUSINESS_user", JSON.stringify(loggedUser));
+        setUser(loggedUser);
       }
     } catch (err) {
-     
       msgs.current.show({
-        sticky: true, severity: 'error',  
-          detail:err.response.data.error.message
-      , closable: true
-      })
+        sticky: true,
+        severity: "error",
+        detail: err.response.data.error.message,
+        closable: true,
+      });
+    }
+  };
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+    let url = "http://localhost:3000/api/logout";
+    const res = await axios.get(url);
+    if (res.data.success) {
+      localStorage.removeItem("SHADY_BUSINESS_user");
+      setUser(null);
+      props.setSignInVisible(false);
+      props.toast.current.show({
+        severity: "success",
+        summary: "Success",
+        detail: "You Logged out successfully",
+        life: 5000,
+      });
     }
   };
   return (
@@ -54,90 +73,161 @@ function SignInSidebar(props) {
         position="right"
         style={{ width: "400px" }}
       >
-        <div className="flex align-items-center justify-content-center">
-          <div className="surface-card p-4 shadow-4 border-round w-full ">
-            <div className="text-center mb-5">
-              <img
-                src="/assets/images/blocks/logos/hyper.svg"
-                height={50}
-                className="mb-3"
-              />
-              <div className="text-900 text-3xl font-medium mb-3">
-                Welcome Back
-              </div>
-              <span className="text-600 font-medium line-height-3">
-                Don't have an account?
-              </span>
-              <a className="font-medium no-underline ml-2 text-blue-500 cursor-pointer">
-                <Link
-                  to="/signup"
-                  className="no-underline"
-                  onClick={() => props.setSignInVisible(false)}
+        {user ? (
+          <div className="flex flex-column">
+            <p className="capitalize font-bold text-xl font-italic mx-auto mb-4 text-primary">
+              {" "}
+              {user.firstName}
+            </p>
+            <div className="border-1 border-dashed my-2"></div>
+            <Button
+              label="Account"
+              text
+              raised
+              className="my-2"
+              onClick={() => {
+                props.setSignInVisible(false);
+                navigate("/account/orders");
+              }}
+            />
+            <Button
+              label="Orders"
+              text
+              raised
+              className="my-2"
+              onClick={() => {
+                props.setSignInVisible(false);
+                navigate("/account/orders");
+              }}
+            />
+            <Button
+              label="Addresses"
+              text
+              raised
+              className="my-2"
+              onClick={() => {
+                props.setSignInVisible(false);
+                navigate("/account/addresses");
+              }}
+            />
+            <Button
+              label="Settings"
+              text
+              raised
+              className="my-2"
+              onClick={() => {
+                props.setSignInVisible(false);
+                navigate("/account/settings");
+              }}
+            />
+            <Button
+              label="Logout"
+              text
+              raised
+              className="my-2"
+              onClick={handleLogout}
+            />
+          </div>
+        ) : (
+          <div className="flex align-items-center justify-content-center ">
+            <div className="surface-card p-4  border-round w-full ">
+              <div className="text-center mb-5">
+                <div
+                  className="bg-primary text-center mx-4 sm:mx-0 p-3 my-5 border-3 shadow-3"
+                  style={{
+                    transform: "skew(15deg)",
+                    borderRadius: " 0px 25px 0px 25px ",
+                  }}
                 >
-                  Create today!
-                </Link>
-              </a>
-            </div>
-
-            <form onSubmit={handleFormSubmit}>
-              
-            <Messages ref={msgs}  className='signin-message'/>
-              
-              <label
-                htmlFor="email"
-                className="block text-900 font-medium mb-2"
-              >
-                Email
-              </label>
-              <InputText
-                id="email"
-                name="email"
-                value={form.email}
-                onChange={handleChange}
-                type="text"
-                placeholder="Email address"
-                className="w-full mb-3"
-                required
-              />
-
-              <label
-                htmlFor="password"
-                className="block text-900 font-medium mb-2"
-              >
-                Password
-              </label>
-
-              <div className="card flex justify-content-center mx-0 my-2 p-0 fluid">
-                <Password
-                value={form.password}
-                onChange={handleChange}
-                name='password'
-                  className="w-full"
-                  toggleMask
-                  feedback={false}
-                  inputStyle={{ width: "100%" }}
-                  required
-                />
-              </div>
-              <div className="flex align-items-center justify-content-between mb-6">
-                <div className="flex align-items-center">
-                  <Checkbox
-                    id="rememberme"
-                    onChange={(e) => setChecked(e.checked)}
-                    checked={checked}
-                    className="mr-2"
-                  />
-                  <label htmlFor="rememberme">Remember me</label>
+                  <p
+                    className="font-bold text-xl text-0 "
+                    style={{ transform: "skew(-15deg" }}
+                  >
+                    Vaporesta
+                  </p>
                 </div>
-                <a className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">
-                  Forgot your password?
+                <p className="text-primary text-3xl font-medium mb-3 ">
+                  Welcome Back
+                </p>
+                <span className="text-600 font-medium line-height-3">
+                  Don't have an account?
+                </span>
+                <a className="font-medium no-underline ml-2 text-primary cursor-pointer">
+                  <Link
+                    to="/signup"
+                    className="no-underline text-primary"
+                    onClick={() => props.setSignInVisible(false)}
+                  >
+                    Create today!
+                  </Link>
                 </a>
               </div>
-
-              <Button label="Sign In" icon="pi pi-user" type="submit" className="w-full" />
-            </form>
+              <form onSubmit={handleFormSubmit}>
+                <Messages ref={msgs} className="signin-message" />
+                <label
+                  htmlFor="email"
+                  className="block text-900 font-medium mb-2"
+                >
+                  Email
+                </label>
+                <InputText
+                  id="email"
+                  name="email"
+                  value={form.email}
+                  onChange={handleChange}
+                  type="text"
+                  placeholder="Email address"
+                  className="w-full mb-3 text-800"
+                  required
+                />
+                <label
+                  htmlFor="password"
+                  className="block text-900 font-medium mb-2"
+                >
+                  Password
+                </label>
+                <div className="card flex justify-content-center mx-0 my-2 p-0 fluid">
+                  <Password
+                    value={form.password}
+                    onChange={handleChange}
+                    name="password"
+                    className="w-full"
+                    toggleMask
+                    feedback={false}
+                    pt={{
+                      input: {
+                        className: "text-800 w-full",
+                      },
+                    }}
+                    required
+                  />
+                </div>
+                <div className="flex flex-column sm:flex-row align-items-start sm:align-items-center justify-content-between mb-6">
+                  <div className="flex align-items-center mb-2">
+                    <Checkbox
+                      id="rememberme"
+                      onChange={(e) => setChecked(e.checked)}
+                      checked={checked}
+                      className="mr-2"
+                    />
+                    <label htmlFor="rememberme" className="text-800">
+                      Remember me
+                    </label>
+                  </div>
+                  <a className="font-medium no-underline m-0 sm:ml-2 text-primary text-right cursor-pointer">
+                    Forgot password
+                  </a>
+                </div>
+                <Button
+                  label="Sign In"
+                  icon="pi pi-user"
+                  type="submit"
+                  className="w-full"
+                />
+              </form>
+            </div>
           </div>
-        </div>
+        )}
       </Sidebar>
     </div>
   );
