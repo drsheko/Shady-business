2909fd54e63e4f005b34d0218e7407be2b22cfed
drsheko <drsheko91@gmail.com>
2023-09-04 01:10:28 -0500

    add decrease sold product quantity

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,7 +1,8 @@
 var Order = require("../models/orderModel");
 var Address = require("../models/addressModal");
 var User = require("../models/userModel");
-
+var Product = require("../models/productModel");
+var Option = require("../models/productOption");
 // CREATE A NEW ORDER
 exports.CREATE_NEW_ORDER = async (req, res) => {
   try {
@@ -52,9 +53,58 @@ exports.CREATE_NEW_ORDER = async (req, res) => {
         ],
       },
     ]);
+    changeProductsQyt(req.body.products)
     return res.status(200).json({ success: true, order });
   } catch (error) {
     return res.status(401).json({ success: false, error });
+  }
+};
+
+const changeProductsQyt = async (products) => {
+  try {
+    products.forEach(async (product) => {
+      if (product.option) {
+     await Option.findByIdAndUpdate(product.option, [
+        {
+          $set: {
+            "total_stock": {
+              $max: [
+                0,
+                {
+                  $subtract: [
+                    "$total_stock",
+                    product.quantity
+                  ]
+                }
+              ]
+            }
+          }
+        }
+      ]);
+      }else{ 
+        
+
+        await Product.findByIdAndUpdate(product.product, [
+          {
+            $set: {
+              "total_stock": {
+                $max: [
+                  0,
+                  {
+                    $subtract: [
+                      "$total_stock",
+                      product.quantity
+                    ]
+                  }
+                ]
+              }
+            }
+          }
+        ]);
+      }
+    });
+  } catch (error) { 
+    throw new Error()
   }
 };
 
@@ -62,7 +112,47 @@ exports.CREATE_NEW_ORDER = async (req, res) => {
 exports.GET_ORDERS_BY_USER_ID = async (req, res) => {
   try {
     let id = req.body.userId;
-    let orders = await Order.find({ user: id }).populate([
+    let orders = await Order.find({ user: id })
+      .populate([
+        {
+          path: "user",
+        },
+        {
+          path: "billingAddress",
+        },
+        {
+          path: "payment",
+        },
+        {
+          path: "shipping",
+          populate: {
+            path: "address",
+          },
+        },
+        {
+          path: "products",
+          populate: [
+            {
+              path: "product",
+            },
+            {
+              path: "option",
+            },
+          ],
+        },
+      ])
+      .sort({ createAt: -1 });
+    return res.status(200).json({ success: true, orders });
+  } catch (error) {
+    return res.status(401).json({ success: false, error });
+  }
+};
+
+// GET ALL ORDERS
+
+exports.GET_ALL_ORDERS = async (req, res) => {
+  try {
+    let orders = await Order.find({}).populate([
       {
         path: "user",
       },
@@ -90,47 +180,9 @@ exports.GET_ORDERS_BY_USER_ID = async (req, res) => {
         ],
       },
     ]);
+
     return res.status(200).json({ success: true, orders });
   } catch (error) {
     return res.status(401).json({ success: false, error });
   }
 };
-
-// GET ALL ORDERS 
-
-exports.GET_ALL_ORDERS = async(req, res) =>{
-try{
-  let orders = await Order.find({}).populate([
-    {
-      path: "user",
-    },
-    {
-      path: "billingAddress",
-    },
-    {
-      path: "payment",
-    },
-    {
-      path: "shipping",
-      populate: {
-        path: "address",
-      },
-    },
-    {
-      path: "products",
-      populate: [
-        {
-          path: "product",
-        },
-        {
-          path: "option",
-        },
-      ],
-    },
-  ]);
-  return res.status(200).json({ success: true, orders });
-
-}catch(error){
-  return res.status(401).json({ success: false, error });
-}
-}
