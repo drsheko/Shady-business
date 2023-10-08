dbacdc2af11b24b84e059644b7d2d1035274b382
drsheko <drsheko91@gmail.com>
2023-10-07 23:31:34 -0500

    Add sign in Form

--- admin-dashboard/src/components/signinPage.jsx
+++ admin-dashboard/src/components/signinPage.jsx
@@ -1,56 +1,109 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { InputText } from "primereact/inputtext";
+import { Password } from "primereact/password";
+import { Checkbox } from "primereact/checkbox";
+import { Button } from "primereact/button";
+import axios from "axios";
+import { UserContext } from "../App";
 function SingInPage(props) {
+    const {user, setUser} = useContext(UserContext)
+  const [form, setFrom] = useState({ email: "", password: "" , rememberMe:false});
+  const [rememberMe, setRememberMe] = useState(false);
+  const [error, setError] = useState(null)
+  const handleFormChange = (e) => {
+    setError(null)
+    let name = e.target.name;
+    let value = e.target.value;
+    setFrom((form) => ({ ...form, [name]: value }));
+  };
+  const submitForm = async(e) =>{
+    e.preventDefault();
+    setError(null)
+    try{
+        let url;
+        let res = await axios.post("http://localhost:3000/api/admin/login", {
+        email: form.email,
+        password: form.password,
+      });
+        if(res.data.success&&res.data.user){
+            setUser(res.data.user);
+            if(rememberMe){
+                localStorage.setItem("SHADY_BUSINESS_ADMIN", JSON.stringify(res.data.user));
+            }
+        }
+    }catch(error){
+        console.log(error)
+        setError(error.response.data.error.message)
+    }
+  }
   return (
-    
-        <div
-          class="px-4 py-8 md:px-6 lg:px-8 signin-page flex justify-content-center align-items-center w-full h-full"
-        >
-          
-            <div class="w-full lg:w-6 p-4 lg:p-7 surface-card opacity-80">
-              <div class="text-900 text-2xl font-medium mb-6">Login</div>
-              <label for="email3" class="block text-900 font-medium mb-2">
-                Email
-              </label>
-              <input
-                id="email3"
-                type="text"
-                placeholder="Email address"
-                class="p-inputtext p-component w-full mb-4"
-              />
-              <label for="password3" class="block text-900 font-medium mb-2">
-                Password
-              </label>
-              <input
-                id="password3"
-                type="password"
-                placeholder="Password"
-                class="p-inputtext p-component w-full mb-4"
-              />
-              <div class="flex align-items-center justify-content-between mb-6">
-                <div class="flex align-items-center">
-                  <div id="rememberme3" class="p-checkbox p-component mr-2">
-                    <div class="p-hidden-accessible">
-                      <input type="checkbox" />
-                    </div>
-                    <div class="p-checkbox-box"></div>
-                  </div>
-                  <label for="rememberme3">Remember me</label>
-                </div>
-                <a class="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">
-                  Forgot password?
-                </a>
-              </div>
-              <button aria-label="Login" class="p-button p-component w-full">
-                <span class="p-button-icon p-c p-button-icon-left pi pi-user"></span>
-                <span class="p-button-label p-c">Login</span>
-                <span
-                  role="presentation"
-                  class="p-ink"
-                  style={{height: "527.2px", width:" 527.2px"}}
-                ></span>
-              </button>
+    <div class="px-4 py-8 md:px-6 lg:px-8 signin-page flex justify-content-center align-items-center w-full h-full ">
+      <div class="w-full lg:w-6 p-4 lg:p-7 surface-card opacity-90 border-round-lg max-w-30rem">
+        <div class="text-900 text-2xl font-semibold mb-6 text-center text-primary ">
+          Login
+        </div>
+        <form onSubmit={submitForm}>
+            {
+                error && <p className="bg-red-500 text-center text-white border-round-lg p-3 font-semibold opacity-100">{error}</p>
+            }
+        <div className="field">
+          <label htmlFor="email" className=" text-900 font-semibold mb-2">
+            Email
+          </label>
+          <InputText
+            id="email"
+            name="email"
+            type="email"
+            value={form.email}
+            onChange={(e) => handleFormChange(e)}
+            placeholder="Email address"
+            className=" w-full mb-4 p-inputtext-sm"
+            required
+          />
+        </div>
+        <div className="field w-full">
+          <label htmlFor="password" className=" text-900 font-semibold mb-2">
+            Password
+          </label>
+          <Password
+            id="password"
+            name="password"
+            value={form.password}
+            onChange={(e) => handleFormChange(e)}
+            feedback={false}
+            toggleMask
+            placeholder="Password"
+            className=" w-full mb-4 "
+            pt={{
+                input:{
+                    className:"w-full p-inputtext-sm"
+                }
+            }}
+            required
+          />
+        </div>
+
+        <div className="flex align-items-center justify-content-between mb-6">
+          <div className="flex align-items-center">
+            <div >
+                <Checkbox 
+                id="rememberme"
+                   checked={rememberMe}
+                    onChange ={(e) => setRememberMe(e.checked)}
+                    className="mr-2"
+                />
+              
             </div>
+            <label htmlFor="rememberme">Remember me</label>
           </div>
+          <a className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">
+            Forgot password?
+          </a>
+        </div>
+        <Button label="Login" icon="pi pi-user" type="submit" className="w-full"/> 
+        </form>
+      </div>
+    </div>
   );
 }
 
